499a71abf3447067da374401e7b7af746ef9f39e
Jayashankara  DM <57224998+Jayashankara-DM-135@users.noreply.github.com>
2022-02-27 09:56:07 +0530

    Update exception_handling.py

--- Advance/Exception Handling/exception_handling.py
+++ Advance/Exception Handling/exception_handling.py
@@ -12,6 +12,12 @@ finally: specify code needs to be executed irrespective of the exception is occu
 
 raise: used to generate custom exception
 """
+
+Note: Don't use final blcok for cleanup file which you opened in try blcok.
+	Ex:
+          if file opening in try block is thrown excpetion due to file not found, then later in finally block if you are trying close the file it will error out.
+	  So closing file should be done at else block , But final block is used do other cleanup, but not file close.
+
 #Example 1
 try:
 	#filename = "simple.txt"
@@ -29,14 +35,14 @@ finally:
 	print("File operations is successfull")
 	f.close()
 
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 print("\n")
 #Example 2
 
 try:
 	f = open("simple.txt")
-	#v = bad_error
-	raise FileNotFoundError
+	raise FileNotFoundError 
 except FileNotFoundError as e:
 	print("File not found exception")
 	print(e)
@@ -46,4 +52,46 @@ else:
 	print("Code which should follow after try block code in case if there is no exception")
 finally:
 	print("Execute this block always")
+	f.close()  # This will give errror if there is exception from try block.
+=============
+
+try:
+	f = open("simple.txt")
+	raise FileNotFoundError 
+except FileNotFoundError as e:
+	print("File not found exception")
+	print(e)
+except Exception as e:
+	print(e)
+else:
+	print("Code which should follow after try block code in case if there is no exception")
+	# do other stufs 
+	f.close()  # This works because 'else' blcok will execute only when there no exception.
+finally:
+	print("Execute this block always")
+	
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+Why this is not a good way writing exception handling code ?
+We should be handling one specific exception using try/exeption/else/finally blcoks.
+
+Even those below code works fine, There are 4(open, write, read and close) source of exception we are hadning in same  try/exeption/else/finally blcoks, this not recommended 
+
+try:
+	f = open("simple.txt")
+	raise FileNotFoundError 
+	f.write("some content")
+	print(f.read())
 	f.close()
+except FileNotFoundError as e:
+	print("File not found exception")
+	print(e)
+except Exception as e:
+	print(e)
+else:
+	print("Code which should follow after try block code in case if there is no exception")
+finally:
+	print("Execute this block always")
+	
+
